ec62f800bc38d7630567afdc792f2fb5436f100b
Aoki-Tomohiro <115062938+Aoki-Tomohiro@users.noreply.github.com>
2024-11-21 15:37:53 +0900

    02_05_確認課題

--- TextureConverter.h
+++ TextureConverter.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include "Externals/DirectXTex/DirectXTex.h"
 
 /// <summary>
 /// テクスチャコンバーター
@@ -21,10 +22,37 @@ private:
 	void LoadWICTextureFromFile(const std::string& filePath);
 
 	/// <summary>
+	/// フォルダパスとファイル名を分離する
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void SeparateFilePath(const std::wstring& filePath);
+
+	/// <summary>
+	/// DDSテクスチャとしてファイル書き出し
+	/// </summary>
+	void SaveDDSTextureToFile();
+
+	/// <summary>
 	/// マルチバイト文字列をワイド文字列に変換
 	/// </summary>
 	/// <param name="mString">マルチバイト文字列</param>
 	/// <returns>ワイド文字列</returns>
 	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
+
+private:
+	//画像の情報
+	DirectX::TexMetadata metaData_;
+
+	//画像イメージのコンテナ
+	DirectX::ScratchImage scratchImage_;
+
+	//ディレクトリパス
+	std::wstring directoryPath_;
+
+	//ファイル名
+	std::wstring fileName_;
+
+	//ファイル拡張子
+	std::wstring fileExt_;
 };
 
