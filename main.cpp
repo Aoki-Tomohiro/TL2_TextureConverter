4d4553290cd220dee4c33bc8cbae10b28fc1a894
Aoki-Tomohiro <115062938+Aoki-Tomohiro@users.noreply.github.com>
2024-11-21 15:13:17 +0900

    02_03_確認課題

--- main.cpp
+++ main.cpp
@@ -1,31 +1,17 @@
-#include <Windows.h>
 #include <cstdio>
 #include <cstdlib>
-#include <cassert>
-#include "TextureConverter.h"
-
-//コマンドライン引数
-enum Argument
-{
-	kApplicationPath, //アプリケーションのパス
-	kFilePath,        //渡されたファイルのパス
-	NumArgument,
-};
 
 int main(int argc, char* argv[])
 {
-	//COMライブラリの初期化
-	assert(argc >= NumArgument);
-	HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
-
-	//テクスチャコンバーター
-	TextureConverter converter;
-
-	//テクスチャ変換
-	converter.ConvertTextureWICToDDS(argv[kFilePath]);
-
-	//COMライブラリの終了
-	CoUninitialize();
+	//argsの数だけ繰り返す
+	for (int i = 0; i < argc; i++)
+	{
+		//文字列argvのi番を表示
+		printf(argv[i]);
+		//改行
+		printf("\n");
+	}
 	system("pause");
+
 	return 0;
 }
